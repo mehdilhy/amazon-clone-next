a0ad2c15f939497bf6e729b60161222766f23e72
Medos <73502818+Medos-dev@users.noreply.github.com>
2021-08-25 22:59:32 +0100

    Banner done

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -1,7 +1,37 @@
 import React from "react";
-
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Links from "./links";
 const Banner = () => {
-  return <div></div>;
+  return (
+    <div className="relative">
+      <div className="absolute w-full h-32 bg-gradient-to-t from-gray-400 to-transparent bottom-0" />
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={2000}
+      >
+        <div>
+          <img loading="lazy" src={Links.first} />
+        </div>
+        <div>
+          <img loading="lazy" src={Links.second} />
+        </div>
+        <div>
+          <img loading="lazy" src={Links.third} />
+        </div>
+        <div>
+          <img loading="lazy" src={Links.fourth} />
+        </div>
+        <div>
+          <img loading="lazy" src={Links.fifth} />
+        </div>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Banner;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import { Text } from "@chakra-ui/react";
 import Head from "next/head";
+import Banner from "../components/Banner/Banner";
+import ProductFeed from "../components/Banner/ProductFeed";
 import Header from "../components/Header/Header";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
-    <div className=" dark:bg-[#131921] transition duration-200">
+    <div className=" bg-gray-200 dark:bg-[#131921] transition duration-200">
       <Head>
         <title>Amazon</title>
         <link rel="icon" href="/favicon.ico" />
@@ -12,7 +14,19 @@ export default function Home() {
       <Header />
       <main className="max-w-screen-2xl mx-auto">
         <Banner />
+        <ProductFeed products={products} />
       </main>
     </div>
   );
+}
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      products,
+    },
+  };
 }
