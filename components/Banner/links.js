ea2c9c6ef3bc302f8f5573fd1a0552368c691501
Medos <73502818+Medos-dev@users.noreply.github.com>
2021-08-25 23:02:15 +0100

    fixed error for build

---
+++ components/Banner/links.js
@@ -0,0 +1,8 @@
+const links = {
+  first: "https://m.media-amazon.com/images/I/711Y9Al9RNL._SX3000_.jpg",
+  second: "https://m.media-amazon.com/images/I/71qid7QFWJL._SX3000_.jpg",
+  third: "https://m.media-amazon.com/images/I/61DUO0NqyyL._SX3000_.jpg",
+  fourth: "https://m.media-amazon.com/images/I/61CiqVTRBEL._SX3000_.jpg",
+  fifth: "https://m.media-amazon.com/images/I/61TD5JLGhIL._SX3000_.jpg",
+};
+export default links;
